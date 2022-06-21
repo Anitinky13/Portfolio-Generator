65dbb4d20d86058b813f4769b5c069462b8a66ce
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-06-20 21:29:11 -0500

    initial

--- app.js
+++ app.js
@@ -1,2 +1,8 @@
+var commandLineArgs = process.argv;
+console.log(commandLineArgs);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
 
-console.log('Hello Node!');
+profileDataArr.forEach((profileItem) => console.log(profileItem));
+
+printProfileData(profileDataArgs);
