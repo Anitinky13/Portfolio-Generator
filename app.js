eebff19cec5284a365117d42fa339e79e068fd19
Anitinky13git config --global user.email ayefanova1@gmail.com <ayefanova1@gmail.com>
2022-06-22 21:11:18 -0500

    html template

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-//const fs = require("fs");
+import fs from "fs";
 import inquirer from "inquirer";
-//const generatePage = require("./src/page-template");
+import generatePage from "./src/page-template.js";
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -133,15 +133,13 @@ promptUser()
   .then(promptProject)
   .then((portfolioData) => {
     console.log(portfolioData);
+    // will be uncommented in lesson 4
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+
+      console.log(
+        "Page created! Check out index.html in this directory to see it!"
+      );
+    });
   });
-
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("./index.html", generatePage(name, github), (err) => {
-//   if (err) throw err;
-
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
